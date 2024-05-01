395eaeb277afbeaa36ce45548d23050c60e38196
Jean <JeanChirinos17@gmail.com>
2024-05-01 18:15:10 -0500

    Refactor udpdateGoogleSession.ts to remove unused code and comments

--- src/controllers/AuthController/udpdateGoogleSession.ts
+++ src/controllers/AuthController/udpdateGoogleSession.ts
@@ -1,10 +1,9 @@
 'use server'
 import { createAuthToken } from '../AuthController/utils/createAuthToken'
-// import { createGroupWithSessionCookie } from '../GroupController/utils/createGroupWithSessionCookie'
+import { createGroupWithSessionCookie } from '../GroupController/utils/createGroupWithSessionCookie'
 
 export async function udpdateGoogleSession(token: string) {
-  // await createAuthToken(token)
-  createAuthToken(token)
+  await createAuthToken(token)
 
-  // createGroupWithSessionCookie()
+  await createGroupWithSessionCookie()
 }
