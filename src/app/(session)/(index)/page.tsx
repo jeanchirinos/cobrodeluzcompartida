7fe8a7f56479cafa1ad7fb9dcc8e0ae637ad3512
Jean <JeanChirinos17@gmail.com>
2024-06-15 18:28:48 -0500

    .

--- src/app/(session)/(index)/page.tsx
+++ src/app/(session)/(index)/page.tsx
@@ -1,18 +1,3 @@
-const jsonObjectOrArray = [
-  {
-    color: 'purple',
-    type: 'minivan',
-    registration: new Date('2017-01-03'),
-    capacity: 7,
-  },
-  {
-    color: 'red',
-    type: 'station wagon',
-    registration: new Date('2018-03-03'),
-    capacity: 5,
-  },
-]
-
 export default function Page() {
-  return <pre>{JSON.stringify(jsonObjectOrArray, null, 2)}</pre>
+  return <></>
 }
