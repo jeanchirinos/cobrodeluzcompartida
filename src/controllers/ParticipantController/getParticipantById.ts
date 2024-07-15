aace7fd4be92e28555bbecc92fea188ff92ba10d
Jean <JeanChirinos17@gmail.com>
2024-07-14 19:37:00 -0500

    add: CCSEC-21 Participant / show

--- src/controllers/ParticipantController/getParticipantById.ts
+++ src/controllers/ParticipantController/getParticipantById.ts
@@ -1,7 +1,7 @@
 import { Participant } from '@/models/Participant'
 import { RentalGroup } from '@/models/RentalGroup'
-// import { API_ROUTE } from '@/constants/api-routes'
-// import { getData } from '@/utilities/request/getData/getData'
+import { API_ROUTE } from '@/constants/api-routes'
+import { getData } from '@/utilities/request/getData/getData'
 
 type ArgsGetParticipantByIdFn = {
   id: Participant['id']
@@ -14,17 +14,9 @@ type ResponseGetParticipantById = Participant & {
 export async function getParticipantById(args: ArgsGetParticipantByIdFn) {
   const { id } = args
 
-  // const participant = await getData<ResponseGetParticipantById>({
-  //   url: API_ROUTE.PARTICIPANT.SHOW({ id }),
-  // })
-
-  const participant: ResponseGetParticipantById = {
-    id,
-    active: true,
-    alias: 'Participant 1',
-    is_main: true,
-    rental_group_id: 1,
-  }
+  const participant = await getData<ResponseGetParticipantById>({
+    url: API_ROUTE.PARTICIPANT.SHOW({ id }),
+  })
 
   return { participant }
 }
