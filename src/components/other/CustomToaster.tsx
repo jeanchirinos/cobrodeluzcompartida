3b30d4c445ed5270b69d3ba42e206dab4ee90e47
Jean <JeanChirinos17@gmail.com>
2025-03-09 22:12:31 -0500

    refactor: Simplify import statements in CustomToaster component

--- src/components/other/CustomToaster.tsx
+++ src/components/other/CustomToaster.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import { useTheme } from 'next-themes'
-import { type UseThemeProps } from 'next-themes/dist/types'
+import { useTheme, type UseThemeProps } from 'next-themes'
 import { Toaster as SonnerToaster, type ToasterProps } from 'sonner'
 
 export function Toaster() {
